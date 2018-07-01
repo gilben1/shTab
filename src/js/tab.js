c80a2c71e274dc1c7e1463c8f12dbeec1c3dfa52
Nick Gilbert <nickgilbert2@gmail.com>
2018-06-30 19:00:05 -0700

    Basic setup for keyboard entry
    tab.html:
        Adds prompt item and loads keycodes file
    
    keycodes.js:
        Adds a constant dictionary to map keycodes to strings
    
    tab.js:
        outputs text to prompt, delete and backspace remove, enter clears

--- src/js/tab.js
+++ src/js/tab.js
@@ -1,2 +1,41 @@
 // The script that controls the tab functionality in tab.html
-alert("tab loaded!");
+  
+const body = document.querySelector('body');
+const prompt = document.querySelector('.prompt');
+
+var promptContent = "";
+
+body.onkeydown = function(evt) {
+    let keyNum = evt.keyCode;
+    let key = keyCodes[evt.keyCode];
+
+    if (key == "enter") {
+        promptContent = "";
+    }
+    else if (key == "delete" || key == "back") {
+        promptContent = promptContent.slice(0, -1);
+    }
+    else {
+        promptContent += key ? key : keyNum;
+    }
+    prompt.innerHTML = promptContent;
+}
+
+
+
+
+/*var foo = {
+    name: "bar",
+    length: 3
+};
+
+
+const thing = {
+    async load() {
+        let getItem = await browser.storage.local.get("foo");
+        console.log(getItem);
+        browser.storage.local.set({foo});
+    }
+};
+
+document.addEventListener('DOMContentLoaded', thing.load);*/
